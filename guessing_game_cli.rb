3f303ce4fa4e08746696bbda125015d534e0cc22
John Doan <johndoan42@gmail.com>
2020-07-22 14:28:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 require 'pry'
 def run_guessing_game
 rand_num = rand(6)+1
-input = gets.chomp.to_s
+input = gets.chomp
 user_num = input.to_i
     if rand_num == user_num
         puts "You guessed the correct number!"
